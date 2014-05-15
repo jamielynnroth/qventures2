8cabd215cb91606149238b6789ea5132ad6436ed
Jamie Roth <roth.j@me.com>
2014-05-15 17:33:52 -0400

    add static pages and express

--- web.js
+++ web.js
@@ -1,18 +1,10 @@
-// Create a HTTP server on port 8000
-// Send plain text headers and 'Hello World' to each client
- 
-var http = require('http');
-var port = process.env.PORT || 8000;
- 
-var counter = 0;
- 
-http.createServer(function (req, res) {
-  	
-	res.writeHead(200, {'Content-Type': 'text/html'}); // prepare response headers
- 
-	res.end('Hello World');
- 
-}).listen(port);
- 
-// console info message
-console.log('Server running at http://127.0.0.1:' + port);
+var express = require('express');
+var app = express();
+
+// app.get('/', function(req, res){
+//   res.send('hello world');
+// });
+
+app.use(express.static(__dirname+"/public"));
+
+app.listen(5000);
